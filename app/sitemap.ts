4215891a8662ef91a774d40781d0b93b35e7e8d3
Nooc <nooc@nooc.me>
2024-03-18 00:53:07 +0800

    Add post urls for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,41 @@
 import { MetadataRoute } from "next";
 import { dictionaryKeys, getDictionary } from "../dictionaries";
+import { posts } from "@/.velite";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const urls = await Promise.all(
-    dictionaryKeys.map(async (lang) => {
-      const dictionary = await getDictionary(lang);
+  const dictionaries = await Promise.all(dictionaryKeys.map(getDictionary));
 
-      return [
-        {
-          url: new URL(dictionary.urls.home, dictionary.meta.baseUrl).href,
-          lastModified: new Date(),
-          changeFrequency: "daily" as const,
-          priority: 1,
-        },
-        {
-          url: new URL(dictionary.urls.works, dictionary.meta.baseUrl).href,
-          lastModified: new Date(),
-          changeFrequency: "daily" as const,
-          priority: 0.8,
-        },
-      ];
-    }),
-  );
+  const basicUrls = dictionaries.map((dictionary) => {
+    return [
+      {
+        url: new URL(dictionary.urls.home, dictionary.meta.baseUrl).href,
+        lastModified: new Date(),
+        changeFrequency: "daily" as const,
+        priority: 1,
+      },
+      {
+        url: new URL(dictionary.urls.posts, dictionary.meta.baseUrl).href,
+        lastModified: new Date(),
+        changeFrequency: "daily" as const,
+        priority: 1,
+      },
+      {
+        url: new URL(dictionary.urls.works, dictionary.meta.baseUrl).href,
+        lastModified: new Date(),
+        changeFrequency: "daily" as const,
+        priority: 0.8,
+      },
+    ];
+  });
 
-  return urls.flat();
+  const postUrls = posts.map((post) => {
+    return {
+      url: new URL(post.seoLink, dictionaries[0].meta.baseUrl).href,
+      lastModified: post.updated || post.date,
+      changeFrequency: "daily" as const,
+      priority: 1,
+    };
+  });
+
+  return [...basicUrls.flat(), ...postUrls];
 }
