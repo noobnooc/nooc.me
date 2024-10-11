67cd3bf470554b9ea336d6450bd44d570bb4ece4
Nooc <nooc@nooc.me>
2024-10-11 17:29:28 +0800

    Remove prettier related configs

--- prettierPluginTailwindcss.js
+++
@@ -1,9 +0,0 @@
-let prettierPluginTailwindcss;
-
-import('prettier-plugin-tailwindcss').then(module => {
-  prettierPluginTailwindcss = module;
-}).catch(err => {
-  console.error('Failed to load prettier-plugin-tailwindcss', err);
-});
-
-module.exports = prettierPluginTailwindcss;
